e1a13cc35a35153dee1f7c5b8033d670a86abb5c
magnuslykke <magn965m@stud.kea.dk>
2023-09-01 13:03:39 +0200

    first commit

---
+++ capitalization3.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const name = "Peter";
+
+console.log(name.toUpperCase());
+
+console.log(name.substring(0, 2).toLowerCase() + name[2].toUpperCase() + name.substring(3).toLowerCase());
+
+// const name3LetterUpper = nameUpper.substring(nameUpper[2].toUpperCaseer());
+
+// console.log(name3LetterUpper);
